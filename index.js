42d0cf4482b935b0e4d4e9099839b51d2a61bb2e
cokoyoh <charlesokoyoh@gmail.com>
2020-09-11 23:23:45 +0300

    Get query params

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const server = http.createServer((req, res ) => {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+  //Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
   //Get the http method
   const method = req.method.toLowerCase();
 
@@ -14,7 +17,7 @@ const server = http.createServer((req, res ) => {
   res.end('hello world\n');
 
   // log the request path
-  console.log(`Request received on path: ${trimmedPath} with method: ${method}`);
+  console.log(`Request received on path: ${trimmedPath} with method: ${method} with query params: ${queryStringObject}`);
 });
 
 server.listen(3000, () => {
