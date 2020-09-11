753da85e3617d6a907f341d55498238972cb4c8d
cokoyoh <charlesokoyoh@gmail.com>
2020-09-11 23:17:41 +0300

    Parse and log request method

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const server = http.createServer((req, res ) => {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+  //Get the http method
+  const method = req.method.toLowerCase();
+
   // send the response
   res.end('hello world\n');
 
   // log the request path
-  console.log(`Request received on path: ${trimmedPath}`);
+  console.log(`Request received on path: ${trimmedPath} with method: ${method}`);
 });
 
 server.listen(3000, () => {
