47c39b1bdfd592ab68485c49b0b799cb6d3293ef
cokoyoh <charlesokoyoh@gmail.com>
2020-09-12 00:12:53 +0300

    Define routing and controllers/handlers

--- index.js
+++ index.js
@@ -28,14 +28,58 @@ const server = http.createServer((req, res ) => {
   req.on('end', () => {
     buffer += decoder.end()
 
-    // send the response
-    res.end('hello world!\n');
+    // chose the handler to handle the request, if one is not found use the not found  handler
+    const chosenHandler = router[trimmedPath] ?? router.notFound;
 
-    // log the request path
-    console.log('Request received with payload ', buffer);
+    //construct the data object to send to the handler
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: buffer
+    }
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, (statusCode, payload) => {
+      // use the status code called back by the handler or default to 200
+      statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+
+      // use the payload called by the handler or default to {}
+      payload = typeof (payload) == 'object' ? payload : {};
+
+      // convert payload to strung
+      const payloadString = JSON.stringify(payload);
+
+      // return the response
+      res.writeHead(statusCode);
+      res.end(payloadString); 
+      
+      // log the request path
+      console.log('Returning this response: ', statusCode, payloadString);
+    })
   })
 });
 
 server.listen(3000, () => {
   console.log(`server listening on localhost:3000...`);
 });
+
+//Define handlers, lately known as controllers
+const handlers = {};
+
+handlers.sample = (data, callback) => {
+  // callback a http status code, and a payload object
+  callback(406, { 'name': 'sample controller' });
+};
+
+// Not found controller
+handlers.notFound = (data, callback) => {
+  callback(404);
+};
+
+// Define a request router
+const router = {
+  'sample': handlers.sample,
+  'notFound' : handlers.notFound
+};
